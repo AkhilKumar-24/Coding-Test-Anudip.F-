483772d5b717eeac77ba174ded242ae930702fe0
Akhil <147305528+AkhilKumar-24@users.noreply.github.com>
2024-12-12 16:38:55 +0530

    Update Reveresed.java

--- Reveresed.java
+++ Reveresed.java
@@ -2,8 +2,13 @@ package akhil;
 
 public class Reveresed {
     public static void main(String[] args) {
+        // Original string
         String str = "Hello World";
+
+        // Reverse the string using StringBuilder
         String reversed = new StringBuilder(str).reverse().toString();
-        System.out.println(reversed);
+
+        // Print the reversed string
+        System.out.println(reversed); // Output: "dlroW olleH"
     }
 }
